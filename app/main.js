c3f1ca2f6b646af31c5dd119b9814e9d56e560e4
lawlerblader <jacob@hamblin.cc>
2015-06-22 18:04:04 -0700

    less camera control, more interest

--- app/main.js
+++ app/main.js
@@ -1,25 +1,35 @@
 TweenMax.to("#logo", 2, {left:window.innerWidth/2-100});
 
-var scene, camera, renderer;
+var scene, camera, renderer, $viewport, height, width;
+var mX, camerMoveY = 0;
 
 window.request
 
-init();
+setViewport($(document.body));
 animate();
+
+function setViewport($el) {
+  $viewport = $el;
+
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  init();
+}
 
 function init() {
   scene = new THREE.Scene();
-  var WIDTH = window.innerWidth - 20;
-  var HEIGHT = window.innerHeight - 20;
+
+  // renderer
 
   renderer = new THREE.WebGLRenderer({antialias:true});
-  renderer.setSize(WIDTH, HEIGHT);
-  document.body.appendChild(renderer.domElement);
+  renderer.setSize(width, height);
+  $viewport.append(renderer.domElement);
 
-  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
-  camera.position.set(0,10,0);
-  camera.near = 5;
-  camera.far = 15;
+  // camera
+
+  camera = new THREE.PerspectiveCamera(50, width / height, 20, 45);
+  camera.position.set(0, 0, 40);
   scene.add(camera);
 
   renderer.setClearColor(0x111111, 1);
@@ -28,23 +38,31 @@ function init() {
   // light.position.set(0,10,0);
   // scene.add(light);
 
+  // light
+
   var light = new THREE.AmbientLight ( 0x404040 );
   scene.add(light);
 
+  $viewport.on('mousemove', onMouseMove);
+
+  function onMouseMove (event) {
+    mX = (event.clientX / window.innerWidth) * 2 - 1;
+  }
 
 
-  var triangles = 16;
+
+  var triangles = 40;
   THREE.triangles = [];
   for (var i = 0; i < triangles; i++) {
-    var radius = Math.random();
+    var radius = Math.random() * 2;
     var geometry = new THREE.TetrahedronGeometry(radius, 0);
     var material = new THREE.MeshDepthMaterial( { } );
     var pyramid = new THREE.Mesh(geometry, material);
     scene.add(pyramid);
-    var x = Math.random() * 6;
-    var y = Math.random() * 6;
-    var z = Math.random() * 6;
-    pyramid.position.set(x - 3, y - 3, z - 3);
+    var x = Math.random() * 24;
+    var y = Math.random() * 24;
+    var z = Math.random() * 24;
+    pyramid.position.set(x - 12, y - 12, z - 12);
     pyramid.rotX = (Math.random() /5) - .1;
     pyramid.rotY = (Math.random() /5) - .1;
     pyramid.timing = Math.floor(Math.random() * 10);
@@ -67,8 +85,12 @@ function init() {
   //     scene.add(collada.scene );
   //   });
 
+  // controls
 
   THREE.controls = new THREE.OrbitControls(camera, renderer.domElement);
+  THREE.controls.center.set (0,0,0);
+  THREE.controls.userRotate = false;
+  THREE.controls.userZoom = false;
 }
 
 function animate() {
@@ -87,6 +109,10 @@ function animate() {
     }
 
   }
+  camera.position.y += Math.cos(camerMoveY) / 50;
+  camerMoveY += 0.02;
+
+  camera.position.x += ((mX * 5) - camera.position.x) * 0.03;
   renderer.render(scene, camera);
   THREE.controls.update();
 }
--- compiled/application.js
+++ compiled/application.js
@@ -364,26 +364,37 @@ THREE.OrbitControls.prototype = Object.create(THREE.EventDispatcher.prototype);
 
 TweenMax.to("#logo", 2, { left: window.innerWidth / 2 - 100 });
 
-var scene, camera, renderer;
+var scene, camera, renderer, $viewport, height, width;
+var mX,
+    camerMoveY = 0;
 
 window.request;
 
-init();
+setViewport($(document.body));
 animate();
+
+function setViewport($el) {
+  $viewport = $el;
+
+  width = window.innerWidth;
+  height = window.innerHeight;
+
+  init();
+}
 
 function init() {
   scene = new THREE.Scene();
-  var WIDTH = window.innerWidth - 20;
-  var HEIGHT = window.innerHeight - 20;
+
+  // renderer
 
   renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(WIDTH, HEIGHT);
-  document.body.appendChild(renderer.domElement);
+  renderer.setSize(width, height);
+  $viewport.append(renderer.domElement);
 
-  camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.1, 20000);
-  camera.position.set(0, 10, 0);
-  camera.near = 5;
-  camera.far = 15;
+  // camera
+
+  camera = new THREE.PerspectiveCamera(50, width / height, 20, 45);
+  camera.position.set(0, 0, 40);
   scene.add(camera);
 
   renderer.setClearColor(1118481, 1);
@@ -392,21 +403,29 @@ function init() {
   // light.position.set(0,10,0);
   // scene.add(light);
 
+  // light
+
   var light = new THREE.AmbientLight(4210752);
   scene.add(light);
 
-  var triangles = 16;
+  $viewport.on("mousemove", onMouseMove);
+
+  function onMouseMove(event) {
+    mX = event.clientX / window.innerWidth * 2 - 1;
+  }
+
+  var triangles = 40;
   THREE.triangles = [];
   for (var i = 0; i < triangles; i++) {
-    var radius = Math.random();
+    var radius = Math.random() * 2;
     var geometry = new THREE.TetrahedronGeometry(radius, 0);
     var material = new THREE.MeshDepthMaterial({});
     var pyramid = new THREE.Mesh(geometry, material);
     scene.add(pyramid);
-    var x = Math.random() * 6;
-    var y = Math.random() * 6;
-    var z = Math.random() * 6;
-    pyramid.position.set(x - 3, y - 3, z - 3);
+    var x = Math.random() * 24;
+    var y = Math.random() * 24;
+    var z = Math.random() * 24;
+    pyramid.position.set(x - 12, y - 12, z - 12);
     pyramid.rotX = Math.random() / 5 - 0.1;
     pyramid.rotY = Math.random() / 5 - 0.1;
     pyramid.timing = Math.floor(Math.random() * 10);
@@ -427,7 +446,12 @@ function init() {
   //     scene.add(collada.scene );
   //   });
 
+  // controls
+
   THREE.controls = new THREE.OrbitControls(camera, renderer.domElement);
+  THREE.controls.center.set(0, 0, 0);
+  THREE.controls.userRotate = false;
+  THREE.controls.userZoom = false;
 }
 
 function animate() {
@@ -445,6 +469,10 @@ function animate() {
       THREE.triangles[i].rotation.y -= THREE.triangles[i].rotY;
     }
   }
+  camera.position.y += Math.cos(camerMoveY) / 50;
+  camerMoveY += 0.02;
+
+  camera.position.x += (mX * 5 - camera.position.x) * 0.03;
   renderer.render(scene, camera);
   THREE.controls.update();
 }
